0b1832c25d1c85bc3a9f9912aad51bb1b5db5550
Monster-Here <yadavbikash179@gmail.com>
2025-02-07 02:41:23 +0530

    Http Method Done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -7,28 +7,57 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456789@localho
 
 db = SQLAlchemy(app)
 
-class task(db.Model):
-    __tablename__ = 'tasks'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String(200), nullable=False)
-    done = db.Column(db.Boolean, default=False)
+class Task(db.Model):
+    task_id = db.Column(db.Integer, primary_key=True)
+    task_title = db.Column(db.String(100), nullable=False)
+    task_description = db.Column(db.String(200), nullable=True)
+    task_status = db.Column(db.String(20), nullable=False)
+
+@app.route('/tasks', methods=['post'])
+def create_task():
+    data = request.get_json()
+    new_task = Task(
+        task_title=data['task_title'],
+        task_description=data.get('task_description', ''),
+        task_status=data['task_status']
+    )
+    db.session.add(new_task)
+    db.session.commit()
+    return jsonify({'message': 'Task created successfully'}), 201
+
+@app.route('/tasks', methods=['GET'])
+def get_all_tasks():
+    tasks = Task.query.all()
+    return jsonify([{
+        'task_id': task.task_id,
+        'task_title': task.task_title,
+        'task_description': task.task_description,
+        'task_status': task.task_status
+    }for task in tasks]), 200
+
+@app.route('/tasks/<int:task_id>', methods=['PUT'])
+def update_task_status(task_id):
+    data = request.get_json()
+    print(f"Received data: {data}")
+    task = Task.query.get_or_404(task_id)
+    print(f"Task before update: {task}")
+    task.task_status = data['task_status']
+    db.session.commit()
+    print(f"Task after update: {task}")
+    return jsonify({'message': 'Task status updated successfully'}), 200
+
+
+@app.route('/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return jsonify({'message': 'Task deleted successfully'}), 200
+
+
 
 with app.app_context():
     db.create_all()
-
-@app.route('/tasks')
-def get_tasks():
-    tasks = task.query.all()
-    task_list = [
-        {
-            'id': task.id,
-            'title': task.title,
-            'done': task.done
-        }
-        for task in tasks
-    ]
-
-    return jsonify({"tasks":task_list}) 
 
 if __name__ == '__main__':
     app.run(debug=True)
