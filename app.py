238100088032c792285813d9db93aa65a06da97e
Monster-Here <yadavbikash179@gmail.com>
2025-02-07 00:29:30 +0530

    Added PostgreSQL and SQLAlchemy setup

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456789@localhost:5432/flask_database'
 
-@app.route('/')
-def home():
-    return "Hello, Bikash! Your Flask app is running successfully. 🚀"
+db = SQLAlchemy(app)
+
+class task(db.Model):
+    __tablename__ = 'tasks'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(200), nullable=False)
+    done = db.Column(db.Boolean, default=False)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/tasks')
+def get_tasks():
+    tasks = task.query.all()
+    task_list = [
+        {
+            'id': task.id,
+            'title': task.title,
+            'done': task.done
+        }
+        for task in tasks
+    ]
+
+    return jsonify({"tasks":task_list}) 
 
 if __name__ == '__main__':
     app.run(debug=True)
